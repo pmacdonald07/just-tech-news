a8edb5ce5bbee53b2a21d2cb85b7be7b71ff42be
Patrick M <patrickmacdonald07@gmail.com>
2022-10-19 13:30:51 -0400

    added comment functionality and incorporated it into user and post routes

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { Post, User, Vote } = require("../../models");
+const { Post, User, Vote, Comment } = require("../../models");
 const sequelize = require("../../config/connection");
 
 // get all users
@@ -22,6 +22,14 @@ router.get("/", (req, res) => {
     order: [["created_at", "DESC"]],
     include: [
       {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attribute: ["username"],
+        },
+      },
+      {
         model: User,
         attributes: ["username"],
       },
@@ -36,7 +44,37 @@ router.get("/", (req, res) => {
 
 router.get("/:id", (req, res) => {
   // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: [
+      "id",
+      "post_url",
+      "title",
+      "created_at",
+      [
+        sequelize.literal(
+          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+        ),
+        "vote_count",
+      ],
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attribute: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
     .then((dbPostData) => {
       if (!dbPostData) {
         res.status(404).json({ message: "No post found with this id" });
@@ -65,31 +103,7 @@ router.post("/", (req, res) => {
 
 // PUT /api/posts/upvote
 router.put("/upvote", (req, res) => {
-  Vote.create({
-    user_id: req.body.user_id,
-    post_id: req.body.post_id,
-  })
-    .then(() => {
-      // then find the post we just voted on
-      return Post.findOne({
-        where: {
-          id: req.body.post_id,
-        },
-        attributes: [
-          "id",
-          "post_url",
-          "title",
-          "created_at",
-          // use raw MySQL aggregate functino query to get a count of how many votes the post has and return it under the name vote_count
-          [
-            sequelize.literal(
-              "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-            ),
-            "vote_count",
-          ],
-        ],
-      });
-    })
+  Post.upvote(req.body, { Vote })
     .then((dbPostData) => res.json(dbPostData))
     .catch((err) => res.json(err));
 });
