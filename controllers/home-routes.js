bcde461f31a8b98e9fae234722a6ca54f7195b97
Patrick M <patrickmacdonald07@gmail.com>
2022-10-25 12:45:35 -0400

    set up log in session and cookies, and log out

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -4,6 +4,8 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
 router.get("/", (req, res) => {
+  console.log(req.session);
+
   Post.findAll({
     attributes: [
       "id",
@@ -45,6 +47,10 @@ router.get("/", (req, res) => {
 });
 
 router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
   res.render("login");
 });
 
